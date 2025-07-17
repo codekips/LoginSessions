6fe8c851bb0772cf03a74b3965a7bcce1fc4848a
aarora7 <abhay_arora@intuit.com>
2025-07-17 19:00:52 +0530

    scaffolding

---
+++ src/main/java/com/abworks/loginsessionmanager/LoginSessionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.abworks.loginsessionmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSessionManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginSessionManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abworks/loginsessionmanager/LoginSessionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abworks.loginsessionmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginSessionManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
